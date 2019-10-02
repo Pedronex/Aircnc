b6f703652e58570a34c9882c960e4bfe06a58c0c
Pedro Soares <39323962+Pedronex@users.noreply.github.com>
2019-10-02 04:51:35 -0300

    Corrigindo alguns erros

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,4 +1,4 @@
-const User = require("../models/Spot");
+const User = require("../models/User");
 const Spot = require("../models/Spot");
 
 module.exports = {
@@ -15,6 +15,7 @@ module.exports = {
     const { user_id } = req.headers;
 
     const user = await User.findById(user_id);
+    console.log(user);
     if (!user) {
       return res.status(400).json({ error: "O Usuario não existe" });
     }
